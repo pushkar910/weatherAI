8758ecfa4dc528e281f22df2041ef86d41b96663
Pushkar Kumar <kmrpushkar09@gmail.com>
2025-02-02 22:06:31 +0530

    Fixed Issues

--- src/main/java/org/pushkar/weatherai/service/AIChatService.java
+++ src/main/java/org/pushkar/weatherai/service/AIChatService.java
@@ -11,7 +11,7 @@ import java.time.Duration;
 @Slf4j
 public class AIChatService {
 
-    private ChatClient chatClient;
+    private final ChatClient chatClient;
 
     AIChatService(ChatClient.Builder builder){
         this.chatClient = builder
